2a96626859e288802b4dcee701e5bd0fc07ae4dc
hmauroy <hmauroy@gmail.com>
2024-04-05 10:35:31 +0200

    pixel_stepper: Implemented velocity-stepper using steps as pixels. Code creates circles. Robot creates squared off circles.

---
+++ A4988_stepper_28BYJ_48_pixel_algorithm/A4988_stepper_28BYJ_48_pixel_algorithm.ino
@@ -0,0 +1,107 @@
+/*
+ * A4988 driver running dual 28BYJ-48 stepper motors.
+ * Using 1/4 stepping. Microstepping needs to be set on the driver. Pull MS2 HIGH, MS1 and MS3 
+ * can be disconnected due to active LOW on the pins.
+ * 2x2 push buttons for driving each motor in each direction.
+ * 
+ * 28BYJ-48 specs
+ * 32 steps/revolution
+ * Gear box: 1:64
+ * Total 32*64 = 2048 steps per revolution
+ * Half-stepping: 4096 steps/revolution
+ * 1/4 stepping: 8192 steps/revolution
+ * 
+ */
+
+// Define stepper motor connections and steps per revolution:
+#define dirXpin 6
+#define dirYpin 7
+#define stepXpin 8
+#define stepYpin 9
+
+unsigned int stepsPerRevolution = 4*2048; // 1/4 stepping => 8192 steps
+float timePerStepBuffer = 500;
+unsigned int timePerStep = 500;
+float pulleyRadius = 33.0;  // radius of pulleys on stepper motors. Determines linear speed.
+float pulleyCircumference = 2*PI*pulleyRadius;
+float stepLength = 62.0 / 5000; // Calibrated: mm/step. ca. 62mm/5000 steps ved 1/4-stepping
+float vmax = 20.0 / stepLength;  // mm/s maximum speed of any axis.
+
+// Variables for positioning
+float x0 = 0;
+float y0 = 0;
+float x = 0;
+float y = 0;
+unsigned int radius = 1;  // pixel coordinates 
+float xspeed = 20.0; // mm/second. 20 mm/s is maximum linear speed.
+float yspeed = 20.0;
+bool lifted = false;
+
+float dx = 0;
+float dy = 0;
+double tx = 0;
+double ty = 0;
+int n_stepsX = 0;
+int n_stepsY = 0;
+
+double timePerStepX = 6000;
+double timePerStepY = 6000;
+
+
+void setup() {
+  // Set pins as output:
+  pinMode(dirXpin, OUTPUT);
+  pinMode(dirYpin, OUTPUT);
+  pinMode(stepXpin, OUTPUT);
+  pinMode(stepYpin, OUTPUT);
+  
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("Startup waits 1000 ms...");
+  Serial.println("*************************************");
+
+  delay(1000);
+  
+ /*
+  // Runs only once
+  int radius = 10;
+  Serial.print("Moves in a circle radius = ");
+  Serial.print(radius);
+  drawCircle(20,20,radius);
+
+  radius = 20;
+  Serial.print("Moves in a circle radius = ");
+  Serial.print(radius);
+  drawCircle(0,0,20);
+
+  radius = 30;
+  Serial.print("Moves in a circle radius = ");
+  Serial.print(radius);
+  drawCircle(0,0,radius);
+*/
+  float r = 30;  // mm coordinates as input
+  Serial.print("Moves in a circle radius = ");
+  Serial.println(r);
+
+  for (int i=0; i<5; i++) {
+    drawCircle(50,50,r);
+  }
+
+  // moveHeadTo(0,80);
+  // moveHeadTo(80,80);
+  // moveHeadTo(80,0);
+  // moveHeadTo(0,0);
+
+  /*
+  moveHeadTo(-20,-20);
+  moveHeadTo(-80,-20);
+  moveHeadTo(-80,-80);
+  moveHeadTo(-20,-20);
+  */
+
+}
+
+void loop() {
+  
+
+}
---
+++ A4988_stepper_28BYJ_48_pixel_algorithm/a4988_functions.ino
@@ -0,0 +1,273 @@
+/*
+ * A4988 functions abstracted away
+ */
+#define PI 3.1415926535897932384626433832795
+
+ // Functions
+void stopMotors() {
+  digitalWrite(stepXpin, LOW);
+  digitalWrite(stepYpin, LOW);
+}
+
+void homeHead() {
+  /*
+   * Moves head to 0,0 (upper left corner).
+   * Moves steppers too much into negative direction.
+   */
+}
+
+void zeroHead() {
+  x0 = 0;
+  y0 = 0;
+}
+
+void liftPen() {
+  // Lift pen.
+  lifted = true;
+}
+
+void lowerPen() {
+  // Lower pen.
+  lifted = false;
+}
+
+void moveHeadTo(float targetX, float targetY) {
+
+  /*
+   Moves to coordinate (x,y), then sets x0 and y0 to these values.
+   1) Calculate longest distance.
+   2) Calculate speed in each axis to reach destination at the same time.
+   3) Move x and y in parallel using different speeds.
+  */
+  float dx = targetX - x0;
+  float dy = targetY - y0;
+  
+  // Calculate distances, number of steps and timePerStep and save to global array.
+  calcTimePerStep(dx, dy);
+
+  String str1 = "Moves head to (x,y): (" + String(targetX) + "," + String(targetY) + ")";
+  Serial.println(str1);
+  String str2 = "dx, dy: " + String(dx) + "," + String(dy);
+  Serial.println(str2);
+
+  String str3 = "nx, ny, timePerStepX, timePerStepY: " + String(n_stepsX) + "," + String(n_stepsY) + "," + String(timePerStepX) + "," + String(timePerStepY);
+  Serial.println(str3);
+
+  runSteppers(n_stepsX, n_stepsY, timePerStepX, timePerStepY);  // nsteps, time per step in microseconds
+
+  // Update real integer coordinates
+  int dx_real = n_stepsX * stepLength;
+  int dy_real = n_stepsY * stepLength;
+  x0 = x0 + dx_real;
+  y0 = y0 + dy_real;
+
+  String str4 = "Head position: x,y: " + String(x0) + "," + String(y0);
+  Serial.println(str4);
+  
+}
+
+void drawCircle(float xcenter, float ycenter, float r) {
+  liftPen();
+  // Move to xy_start.
+  //moveHeadTo(xcenter+radius, ycenter); // NB! + radius of circle!
+  // Move along circle of radius r.
+  // 1) Divide circle into segments based on lengt of circumference and resolution limit.
+  // Length og line segment = 5 mm for first try.
+  float segment_length = 5;  // Default 24 for circle with r = 30 mm
+  float n_segments = (2*PI*r)/segment_length;
+  byte n = ceil(n_segments);
+  Serial.print("n_segments Circle: ");
+  Serial.println(n);
+  float d_theta = 2*PI/n;
+  float theta = 0;
+
+  // Initialize x0 and y0, radius as pixel values
+  unsigned int x0_px = round(xcenter / stepLength); // Pixel-position of center of circle.
+  unsigned int y0_px = round(ycenter / stepLength);
+  radius = round(r / stepLength);
+  x0 = x0_px + radius;
+  y0 = y0_px;
+
+
+  // 2) Run for loop of n line segments.
+  //n = 3;
+  for (byte i=0; i < n; i++) {
+    /*
+    Serial.println("--------------");
+    Serial.print("segment: ");
+    Serial.print(i+1);
+    Serial.print(": ");
+    String positions = "x0,y0: " + String(x0) + "," + String(y0);
+    Serial.println(positions);*/
+    theta = theta + d_theta;
+    x = radius * cos(theta) + x0_px;
+    y = radius * sin(theta) + y0_px;
+    String target = String(x) + "," + String(y);
+    //Serial.println(target);
+    dx = x-x0;
+    dy = y-y0;
+    // Calculate distances (number of steps) and timePerStep and save to global variables..
+    calcTimePerStep(dx, dy);
+    //String message = "dx, dy " + String(dx) + "," + String(dy);
+    //Serial.println(message);
+
+    // Run steppers using speeds vx and vy which controls the timing of the steppers.
+
+    String temp2 = "nx, ny, timePerStepX,timePerStepY: " + String(n_stepsX) + "," + String(n_stepsY) + "," + String(timePerStepX) + "," + String(timePerStepY);
+    Serial.println(temp2);
+
+    runSteppers(n_stepsX, n_stepsY, timePerStepX, timePerStepY);  // nsteps, time per step in microseconds
+
+    // Update real integer coordinates
+
+    x0 = x0 + dx; // Purely theoretic positions.
+    y0 = y0 + dy;
+    
+    //x0 = x0 + n_stepsX; // This is correct, but may wreck the drawing.
+    //y0 = y0 + n_stepsY;
+    
+
+  }
+
+  String str4 = "Head position after Circle: x,y: " + String(x0) + "," + String(y0);
+  Serial.println(str4);
+}
+
+void calcTimePerStep(float dx, float dy) {
+  /*
+    Calculates distances, nsteps + timings.
+    Saves to global variables.
+  */
+  n_stepsX = round(dx);
+  n_stepsY = round(dy);
+  timePerStepX = 0;
+  timePerStepY = 0;
+
+  // Calculates distance and preserves the linear speed along line.
+  double s = sqrt(dx*dx + dy*dy);
+  double t = s/vmax;
+  double vx = dx/t;
+  double vy = dy/t; // px/s
+
+  String speedInfo = "s, t, vx, vy: " + String(s) + "," + String(t) + ","+ String(vx) + ","+ String(vy);
+  //Serial.println(speedInfo);
+
+
+  // Calculates n_steps in each axis
+  //String buffer = "dx, dy,vx,vy: " + String(dx) + "," + String(dy) + "," + String(vx) + "," + String(vy);
+  //String buffer = "dx, dy: " + String(dx) + "," + String(dy);
+  String buffer = "n_stepsX, n_stepsY: " + String(n_stepsX) + "," + String(n_stepsY);
+  //Serial.println(buffer);
+ 
+
+  // Calculates timePerStep in each axis
+  /*float temp = (1000*tx)/float(n_stepsX);
+  Serial.print("temp timePerStep (ms): ");
+  Serial.println(temp);*/
+  if (n_stepsX != 0) {
+    timePerStepX = floor(1000*(1000*t / float(n_stepsX)));
+  }
+  if (n_stepsY != 0) {    
+    timePerStepY = floor(1000*(1000*t / float(n_stepsY)));
+  }
+
+ 
+    
+} // END calcTimePerStep()
+
+/* Controls both stepper motors in parallell.
+nx: number of steps in x-axis, negative value is counter clockwise.
+ny: number of steps in y-axis, negative value is counter clockwise.
+timePerStepX: time per step in microseconds for x-stepper.
+timePerStepY: time per step in microseconds for ystepper.
+*/ 
+void runSteppers(int nx, int ny, double timePerStepX, double timePerStepY) {
+  String dirX = "CW";
+  String dirY = "CCW";
+  int pauseX = int(round(timePerStepX/2));  // Divide by 2 because step signal is 2 x delayMicroseconds.
+  int pauseY = int(round(timePerStepY/2)); 
+  if (nx > 0) {
+    digitalWrite(dirXpin, HIGH); // Set the spinning direction clockwise (x- and y-axis are wired opposite at the moment).
+    dirX = "CW";
+  }
+  else if (nx < 0) {
+    digitalWrite(dirXpin, LOW); // CCW
+    dirX = "CCW";
+  }
+  else {
+    dirX = "STILL";
+  }
+  if (ny > 0) {
+    digitalWrite(dirYpin, LOW); // Set the spinning direction counter clockwise
+    dirY = "CCW";
+  }
+  else if (ny < 0) {
+    digitalWrite(dirYpin, HIGH); // CCW
+    dirY = "CW";
+  }
+  else {
+    dirY = "STILL";
+  }
+  // Takes absolute values of both nsteps and pauseTime.
+  nx = abs(nx);
+  ny = abs(ny);
+  pauseX = abs(pauseX);
+  pauseY = abs(pauseY);
+  /*Serial.print("nx, ny, pauseX, pauseY, dirX, dirY: ");
+  String temp3 = String(nx) + "," + String(ny) + "," + String(pauseX)+ "," + String(pauseY) + "," + dirX + "," + dirY;
+  Serial.println(temp3);
+  Serial.println("---------");*/
+  
+  
+  // Spin the steppers as long as there are steps.
+  bool isRunning = true;
+  bool sigX = true;
+  bool sigY = true;
+  unsigned long txLast = micros(); // time in microseconds. overflows in around 70 min.
+  unsigned long tyLast = micros();
+  unsigned long delta_tx = 0;
+  unsigned long delta_ty = 0;
+  while (isRunning) {
+    delta_tx = micros() - txLast;
+    delta_ty = micros() - tyLast;
+    // Check x-axis for signal.
+    if (delta_tx >= pauseX) {
+      if (nx > 0) {
+        sigX = !sigX;
+        digitalWrite(stepXpin, sigX);
+        txLast = micros();
+        if (sigX == false) {
+          nx -= 1;
+        }
+      }
+      else {
+        //Serial.print("X finished, time:");
+        //Serial.println(micros());
+        if (ny == 0) {
+          isRunning = false;
+        }
+      }
+    }
+    // Check y-axis for signal
+    if (delta_ty >= pauseY) {
+      if (ny > 0) {
+        sigY = !sigY;
+        digitalWrite(stepYpin, sigY);
+        tyLast = micros();
+        if (sigY == false) {
+          ny -= 1;
+        }
+      }
+      else {
+        //Serial.print("Y finished, time:");
+        //Serial.println(micros());
+        if (nx == 0) {
+          isRunning = false;
+        }
+      }
+    }
+
+  } // End while loop
+  //Serial.println("Finished runSteppers()");
+} // END runSteppers()
+
